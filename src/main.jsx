31826615cd12aa08e933a46243c54db7b87f4ef3
Afaaq <afaaqmajeed123@gmail.com>
2022-06-15 11:11:20 +0530

    added

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
 import store from './app/store'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+
+);
+
+
+
+
+// const initialState={
+//   nocakes:15
+// }
+
+
+
+// createSlice({
+//   name:'myslice',
+//   initialState,
+
+//   reudcers:{
+//     firstReducer:(state,action)=>{
+//       state.nocakes+=1
+
+//     },
+//     secondReducer:(state,action)=>{
+//       state.nocakes+=action.payload
+
+
+//     }
+
+//   }
+// })
+// useSelector((state)=>state.)
