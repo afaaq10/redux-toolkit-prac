3dbaad1396b88aab7f74fc206eb5a559d7b7bbcb
Afaaq <afaaqmajeed123@gmail.com>
2022-06-16 18:53:06 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './App.css'
 // import { CakeView } from './View'
 // import { IcecreamView } from './features/icecream/IcecreamView'
 import { CakeView } from './View'
-import View2 from './View2'
+
 // import Slice from './Slice'
 function App() {
   return (
     <div className='App'>
     
       <CakeView />
-      <View2 />
+   
       {/* 
       <IcecreamView />
       <UserView /> */}
